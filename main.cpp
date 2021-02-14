ae8d49c6c048723b03735ac2e1d284d8330d4d9f
Akash Singh <32004729+akashsinghx@users.noreply.github.com>
2021-02-14 22:57:19 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,99 @@
+int main()
+{
+string pan,gnd,fn,n,an,bs;
+string l;
+double loan_amount,interest_rate,number_of_years,total_amount,monthly_amount, cs;
+cout<<"Gender"<<endl;
+cin>>gnd;
+do
+{
+if(gnd=="male"||gnd=="female"||gnd=="MALE"||gnd=="FEMALE")
+{
+break;  
+}
+else
+{
+cout<<"TRY AGAIN"<<endl;
+cin>>gnd;
+}   
+}
+while(gnd!="male"||gnd!="female"||gnd!="MALE"||gnd!="FEMALE");   
+cout<<"Full name"<<endl;
+cin>>fn;
+cout<<"Enter 10 digit mobile number"<<endl;
+cin>>n;
+phno(n);
+cout<<"Enter pan"<<endl;
+cin>>pan;
+bool res=isValidPanCardNo(pan);
+if(res==true)
+else
+{
+cout<<"Enter valid pan"<<endl;
+cin>>pan;
+isValidPanCardNo(pan);
+}
+cout<<"Enter Aadhaar Number"<<endl;
+cin>>an;
+aadhaar(an);
+cout<<"Eligibility:"<<endl;
+cout<<"Are you Eligibile with all paramenters of form 16"<<endl;
+cout<<"YES OR NO"<<endl;
+cin>>l;   
+if(l=="yes" || l=="YES")
+{
+cout<<"proceed"<<endl;
+}
+else
+{
+cout<<"reject";
+exit(0);  
+}
+cout<<"Do you have pervious 2 months bank slip"<<endl;
+cout<<"YES OR NO"<<endl;
+cin>>bs;
+if(bs=="yes" || bs=="YES" )
+{
+cout<<"Configuration you may proceed" <<endl;
+}
+else
+{
+cout<<"reject";
+exit(0);  
+}
+cout<<"enter your cibil score"<<endl;
+cin>>cs;
+cout<<endl;
+if((cs>=300) && (cs<=900))
+{
+cout<<"loan approved"<<endl;
+}
+else
+{
+cout<<"loan rejected"<<endl;
+exit(0);
+}
+cout<<"Enter the loan amount: ";
+cin>>loan_amount;
+cout<<endl;
+cout<<"Enter the interest rate: ";
+cin>>interest_rate;
+if(interest_rate>0)
+{
+cout<<interest_rate;
+}
+else
+{
+cout<<"error";
+}
+cout<<endl;
+cout<<"The number of years: ";
+cin>>number_of_years;
+cout<<endl;
+total_amount=(number_of_years*loan_amount)+(number_of_years*loan_amount*(interest_rate/100.00));
+monthly_amount=total_amount/(number_of_years*12);
+cout<<"Total amount to be paid: "<<total_amount<<endl;
+cout<<"Total interest: "<<total_amount-(number_of_years*loan_amount)<<endl;
+cout<<"Monthly amount to be paid: "<<monthly_amount<<endl;
+return 0;
+}
